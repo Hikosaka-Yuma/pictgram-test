f48dd5de7088efe5c4774d889dfc4cf48dbbcb43
Hikosaka_Yuma <uuuumaaa@gmail.com>
2020-03-09 06:33:04 +0000

    validation test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
 validates :name, presence: true, length: {maximum: 15}
 validates :email, presence: true, uniqueness: true
-validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+validates :email, format: { with:"/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i" }
 
 validates :password, presence: true, uniqueness: true
 #パスワードの文字数のバリデーション
-#validates :password, length: {maximum:32, minimum:8,}
+validates :password, length: {maximum:32, minimum:8,}
 #パスワード文字数字を混合させるバリデーション
-#validates :passoword, format: {with: /^[A-Za-z0-9]+S/}
+validates :passoword, format: {with: "/\A[A-Za-z0-9]+\z/"}
 
 has_secure_password
 end
